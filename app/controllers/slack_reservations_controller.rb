4f1447528a7c632899bb689afe6d3f48ca474ecd
mmmirecki <mirecki.michal@gmail.com>
2021-11-13 19:01:19 +0100

    slack slash command

--- app/controllers/slack_reservations_controller.rb
+++ app/controllers/slack_reservations_controller.rb
@@ -6,16 +6,13 @@ class SlackReservationsController < ApplicationController
     if @parking_spot.reservations.exists?
       if @parking_spot.reservations.last.reservation_is_valid?
         @response_message = "This spot is taken"
-        redirect_to root_path, alert: @response_message
       else
         @reservation = @parking_spot.reservations.create(reservation_params)
         @response_message = "Your reservation has been made"
-        redirect_to root_path, alert: "Your reservation has been made #{params[:reservation][:user_name]}"
       end
     else
       @response_message = "Your reservation has been made"
       @reservation = @parking_spot.reservations.create(reservation_params)
-      redirect_to root_path, alert: "Your reservation has been made #{params[:reservation][:user_name]}"
     end
 
     render json: {
